52de0107c2dbe5f44835876085125312cc76cf76
dave <dwtmekonnen123@gmail.com>
2022-10-22 17:28:48 +0300

    feat: setup express boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+const app: Express = express();
 
-export const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'hello world' });
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-import { app } from './app';
-console.log(app);
+import app from './app';
+import http from 'http';
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8001;
+
+server.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
