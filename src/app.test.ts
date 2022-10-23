2119b843f58ec704e0df1591dbf5d9fa3e576932
dave <dwtmekonnen123@gmail.com>
2022-10-23 18:17:47 +0300

    refactor: descriptive test message

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,8 +2,8 @@ import request from 'supertest';
 
 import app from './app';
 
-describe('GET /', () => {
-  it('returns hello world on / request', async () => {
+describe('/ route', () => {
+  it('GET returns hello world on / request', async () => {
     const res = await request(app).get('/');
     const expected = { message: 'hello world' };
     expect(expected).toMatchObject(res.body);
