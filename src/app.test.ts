639bcffdb1e3bc5b9b67abc7463385e1e1d7e0ba
Yagth <yabuman46@gmail.com>
2022-10-28 16:36:59 +0300

    Ref: Changed the expected message of the test

--- src/app.test.ts
+++ src/app.test.ts
@@ -5,7 +5,7 @@ import app from './app';
 describe('/ route', () => {
   it('GET returns hello world on / request', async () => {
     const res = await request(app).get('/');
-    const expected = { message: 'hello world' };
+    const expected = { message: 'Not authorized' };
     expect(expected).toMatchObject(res.body);
   });
 });
