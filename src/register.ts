5cf407aa130cb21e71bb57e89191a5b53493e42c
dave <dwtmekonnen123@gmail.com>
2022-11-03 16:53:34 +0300

    style: linting

--- src/register.ts
+++ src/register.ts
@@ -1,26 +1,26 @@
-import {Request} from 'express';
+import { Request } from 'express';
 import bcrypt from 'bcrypt';
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
 
-async function createUser(req: Request){
+const prisma = new PrismaClient();
 
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = {
-            email: req.body.email,
-            password: hashedPassword,
-            firstName: req.body.firstName,
-            secondName: req.body.secondName,
-        };
-        await prisma.user.create({
-            data:user,
-        });
+async function createUser(req: Request) {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = {
+      email: req.body.email,
+      password: hashedPassword,
+      firstName: req.body.firstName,
+      secondName: req.body.secondName,
+    };
+    await prisma.user.create({
+      data: user,
+    });
 
-        return user;
-    } catch{
-        return null;
-      }
+    return user;
+  } catch {
+    return null;
+  }
 }
 
 export default createUser;
