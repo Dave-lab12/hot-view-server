730504e53fb5bb172f5d4bc2fcc2d2c71c808645
dave <dwtmekonnen123@gmail.com>
2022-11-05 15:21:16 +0300

    fix: updated config file

---
+++ config/default.ts
@@ -0,0 +1,17 @@
+// const env = process.env.NODE_ENV; // 'dev' or 'test'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dev = {
+  app: {
+    host: 'localhost',
+    port: 8001,
+    accessTokenExpiresIn: 15,
+    environment: {
+      accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
+      accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY,
+    },
+  },
+};
+export default dev;
