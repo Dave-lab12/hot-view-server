bf10bafa327ee56d6b191d53c184805f88abf22b
dave <dwtmekonnen123@gmail.com>
2022-11-05 16:23:27 +0300

    refactor: updating configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import http from 'http';
 
-import config from 'config';
+import config from '../config/default';
 
 import app from './app';
 
 const server = http.createServer(app);
 
-const PORT = config.get('server.port');
+const PORT = config.app.port;
 
 server.listen(PORT);
