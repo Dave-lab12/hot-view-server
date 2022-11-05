5aec60de74371c8ae1e1d24e6c02de981227bfdb
dave <dwtmekonnen123@gmail.com>
2022-11-05 16:35:11 +0300

    fix: updated jwt sign function and verify

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,14 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
-import config from 'config';
+
+import config from '../../config/default';
 
 export const signJwt = (
   payload: Record<string, unknown>,
   options: SignOptions = {}
 ) => {
-  const privateKey: string = config.get('environment.accessTokenPrivateKey');
+  // const { server } = config;
+  // console.log(accessTokenPublicKey);
+  const privateKey = config.app.environment.accessTokenPrivateKey as string;
 
   return jwt.sign(payload, privateKey, {
     ...(options && options),
@@ -13,11 +16,23 @@ export const signJwt = (
   });
 };
 
-export const verifyJwt = <T>(token: string): T | null => {
+// export const verifyJwt = <T>(token: string): T | null => {
+//   try {
+//     const publicKey: string = config.get('environment.accessTokenPublicKey');
+//     return jwt.verify(token, publicKey) as T;
+//   } catch (err) {
+//     return null;
+//   }
+// };
+export const verifyJwt = (token: string) => {
   try {
-    const publicKey: string = config.get('environment.accessTokenPublicKey');
-    return jwt.verify(token, publicKey) as T;
-  } catch (err) {
-    return null;
+    const publicKey = config.app.environment.accessTokenPublicKey as string;
+    const decoded = jwt.verify(token, publicKey);
+    return { payload: decoded, expired: false };
+  } catch (error) {
+    return {
+      payload: null,
+      expired: (error as Error).message.concat('jwt expired'),
+    };
   }
 };
