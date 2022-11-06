8f84e7ba9b50e8629f3f2bf5967121ff0812bedd
dave <dwtmekonnen123@gmail.com>
2022-11-06 09:26:55 +0300

    fix: access token expire time

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-process-env */
 // const env = process.env.NODE_ENV; // 'dev' or 'test'
 import dotenv from 'dotenv';
 
@@ -7,7 +8,7 @@ const dev = {
   app: {
     host: 'localhost',
     port: 8001,
-    accessTokenExpiresIn: 15,
+    accessTokenExpiresIn: '1h',
     environment: {
       accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
       accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY,
