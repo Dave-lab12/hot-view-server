779beb852e964d2ea3e4d57b469ac3bacbebe26c
Yagth <yabuman46@gmail.com>
2022-11-15 16:06:15 +0300

    fix: Typo in usershcema

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -6,10 +6,10 @@ export const createUserSchema = z.object({
       firstName: z.string({ required_error: 'first name is required' }),
       lastName: z.string({ required_error: 'last name is required' }),
       email: z
-        .string({ required_error: 'last name is required' })
+        .string({ required_error: 'email is required' })
         .email('Invalid email'),
       password: z
-        .string({ required_error: 'last name is required' })
+        .string({ required_error: 'password is required' })
         .min(8, 'Password must be more than 8 characters')
         .max(32, 'Password must be less than 32 characters'),
       passwordConfirm: z.string({
