6386ba42faa1d37a73ba1ade5afc5d6df799725a
dave-lab12 <dwtmekonnen123@gmail.com>
2022-12-09 11:22:13 +0300

    fix: updated redis url

--- config/default.ts
+++ config/default.ts
@@ -8,7 +8,9 @@ const dev = {
   app: {
     host: 'localhost',
     port: process.env.SERVER_PORT || 8001,
-    REDIS_URL: `${process.env.REDIS_HOST}://${process.env.REDIS_USER}:${process.env.REDIS_PORT}`,
+    REDIS_URL:
+      process.env.REDIS_URL ||
+      `${process.env.REDIS_HOST}://${process.env.REDIS_USER}:${process.env.REDIS_PORT}`,
     NODE_ENV: process.env.NODE_ENV,
   },
 };
